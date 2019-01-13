e9311db7a15d5318f9252c4788e44dfa4fe08b1f
Arimateia Morais <arimateiamf@gmail.com>
2019-01-13 21:27:17 -0200

    Fix print to have a better visual experience

--- src/Main.ts
+++ src/Main.ts
@@ -8,27 +8,27 @@ class main{
         while(opcao != "e"){
             switch(opcao){
                 case "c":
-                    var id:number = readline.question("enter the ip");
-                    var name:string = readline.question("enter the name");
+                    var id:number = readline.question("enter the id\n");
+                    var name:string = readline.question("enter the name\n");
                     controller.createSer(id,name);
                     console.log("a new ser was created")
                     break;
                 case "u":
-                    var id:number = readline.question("enter the ip");
-                    var name:string = readline.question("enter the new name");
+                    var id:number = readline.question("enter the id\n");
+                    var name:string = readline.question("enter the new name\n");
                     controller.updateSer(id,name);
-                    console.log("ser with the ip: " + id + "updated")
+                    console.log("ser with the ip: " + id + " updated")
                     break;
                 case "d":
-                    var id:number = readline.question("enter the ip");
+                    var id:number = readline.question("enter the id\n");
                     controller.deleteSer(id);
-                    console.log("ser with the ip: " + id + "deleted");
+                    console.log("ser with the ip: " + id + " deleted");
                     break;
                 case "a":
                     console.log(controller.readAll());
                     break
                 case "1":
-                    var id:number = readline.question("enter the ip");
+                    var id:number = readline.question("enter the id\n");
                     console.log(controller.readOne(id));
                     break
                 
